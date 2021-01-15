9e4021171213365425561cc7d6a26733071ce9a0
ir-afn <46682788+ir-afn@users.noreply.github.com>
2021-01-15 19:13:18 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const signupBtn = document.querySelector('.form__button');
 const password = document.querySelector('.form__password');
+const showPassword = document.querySelector('.password__button');
 
-signupBtn.onclick = function () {
-    let passLength = password.value.length;
-    if (passLength <= 5) {
-        password.style.borderColor = 'red';
-        password.style.borderWidth = '2px';
+signupBtn.onclick = function (event) {
+  let passLength = password.value.length;
+  if (passLength <= 5) {
+      password.style.borderColor = 'red';
+      password.style.borderWidth = '2px';
+      event.preventDefault();
+    } else {
+      password.style.borderColor = 'green';
+      password.style.borderWidth = '2px';
     }
 }
 
+showPassword.onclick = function () {
+  if (password.type === 'password') {
+      password.type = 'text';
+      showPassword.classList.add('hide-password');
+    } else {
+      password.type = 'password';
+      showPassword.classList.remove('hide-password');
+    }
+}
 
 
 
