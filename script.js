c12015e35187a9d0fb74a5385483bccaec95e748
ir-afn <46682788+ir-afn@users.noreply.github.com>
2021-01-16 21:24:09 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const signupBtn = document.querySelector('.form__button');
 const password = document.querySelector('.form__password');
 const showPassword = document.querySelector('.password__button');
+const passWarning = document.querySelector('.password-warning');
 
 signupBtn.onclick = function (event) {
   let passLength = password.value.length;
   if (passLength <= 5) {
       password.style.borderColor = 'red';
       password.style.borderWidth = '2px';
+      passWarning.style.display = 'block';
       event.preventDefault();
     } else {
       password.style.borderColor = 'green';
       password.style.borderWidth = '2px';
+      passWarning.style.display = 'none';
     }
 }
 
@@ -23,3 +26,26 @@ showPassword.onclick = function () {
       showPassword.classList.remove('hide-password');
     }
 }
+
+
+
+/*
+showPassword.onchange = function () {
+    if (showPassword.checked) {
+      password.type = 'text';
+    } else {
+      password.type = 'password';
+    }
+  };
+  
+  password.oninput = function () {
+    let passLength = password.value.length;
+    securityBar.style.width = passLength * 10 + '%';
+    if (passLength <= 5) {
+      securityBar.style.backgroundColor = 'red';
+    } else if (passLength > 5 && passLength < 10) {
+      securityBar.style.backgroundColor = 'gold';
+    } // Добавьте else сюда
+  
+  };
+*/
